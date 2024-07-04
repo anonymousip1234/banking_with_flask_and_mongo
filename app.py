8eb5036d8a7d4f7fd4e63723a0f5c8f534fe986e
Swapnonil <swapnonilmukherjee62@gmail.com>
2024-07-04 12:37:03 +0530

    modified main flask app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 import os
 
+
+
 load_dotenv()
 
 app=Flask(__name__)
@@ -11,9 +13,7 @@ jwt=JWTManager(app)
 
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-from defined_apis.authenticate import register,login,home
-from defined_apis.accounts import create_account,view_account_list
-from defined_apis.transactions import create_transaction,view_transaction_history
+from defined_apis import *
 
 
 if __name__== "__main__":
